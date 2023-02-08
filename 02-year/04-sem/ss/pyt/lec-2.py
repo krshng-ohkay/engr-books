81553a1c46fb5bca658856b5e46762319a21c5b4
krshng-ohkay <krishuukey2003@gmail.com>
2023-02-08 09:59:25 +0530

    sem 4 first upload

---
+++ 02-year/04-sem/ss/pyt/lec-2.py
@@ -0,0 +1,48 @@
+#////////////////#
+print ("")
+#////////////////#
+
+# Define 3 strings
+
+string1 = "i am from class D9A"
+string2 = "This is D9A's class"
+string3 = '''He said "I am in D9A"'''
+
+print (string1)
+print (string2)
+print (string3)
+
+#////////////////#
+print ("")
+#////////////////#
+
+# Slicing strings
+
+print (string1[0:6])
+print (string2[6:11])
+print (string3[11:])
+print (string1[:16])
+print (string2[0:9:2])
+print (string3[::-1])
+# string1[0] = "F" :: gives an error; strings are immutable.
+#////////////////#
+print ("")
+#////////////////#
+
+# String functions
+
+print (string1.capitalize())
+string4 = string1 + string2
+print (string4)
+
+#////////////////#
+print ("")
+#////////////////#
+
+# Lists
+
+list1 = [1, 2, 5, 10]
+print (list1)
+
+list2 = ['tofu', 'extc', 'D9A']
+print (list2)
